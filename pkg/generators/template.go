7eeeea15ed52ad7ea69369dd7f21f4a1c4968c0d
Juan Manuel Villacorta Campos <juanvillacortac@gmail.com>
2022-03-24 15:07:56 +0000

    New helper

--- pkg/generators/template.go
+++ pkg/generators/template.go
@@ -10,19 +10,25 @@ import (
 
 func templateHelpers(models ast.ModelMap, options GenerateConfig) template.FuncMap {
 	return template.FuncMap{
-		"Models":         func() ast.ModelMap { return models },
-		"Model":          func(modelName string) *ast.Model { return models[modelName] },
-		"ModelDeps":      models.ModelDependencies,
-		"NodeOption":     ast.GetNodeOption,
+		"Models":     func() ast.ModelMap { return models },
+		"Model":      func(modelName string) *ast.Model { return models[modelName] },
+		"ModelDeps":  models.ModelDependencies,
+		"NodeOption": ast.GetNodeOption,
+		"NodeOptionOr": func(n ast.Node, optionName, fallback string) string {
+			if v := ast.GetNodeOption(n, optionName); v != nil {
+				return *v
+			}
+			return fallback
+		},
 		"CamelCase":      strcase.ToCamel,
 		"LowerCamelCase": strcase.ToLowerCamel,
 		"KebabCase":      strcase.ToKebab,
 		"SnakeCase":      strcase.ToSnake,
-		"Plural":      func(str string) string {
+		"Plural": func(str string) string {
 			p := pluralize.NewClient()
 			return p.Plural(str)
 		},
-		"Singular":      func(str string) string {
+		"Singular": func(str string) string {
 			p := pluralize.NewClient()
 			return p.Singular(str)
 		},
